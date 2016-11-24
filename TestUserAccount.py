4956dd4a29916799aaa3a0650aa10e143c1f873c
amirsh18-meet <amos17@meet.mit.edu>
2016-11-24 19:57:18 +0200

    everything

--- TestUserAccount.py
+++ TestUserAccount.py
@@ -2,12 +2,12 @@
 import UserAccount
 
 #Two things are missing from the line below - fill them in
-my_user=UserAcount(  ,  )
+my_user=UserAccount("Ben","0802","my secret is..." )
 
 #Call the print_password method (function) - it takes one input - a guess for the password.
-
+print_password=input(self.password)
 #Use the wrong password as input here
-my_user.
+my_user.password("00234")
 
 #Use the right password here
-my_user.
+my_user.password("0802")

81e90a382d92362cd0ae4c3976477f058079a0fc
ben18-meet <benlevi1905@gmail.com>
2017-01-11 19:57:16 +0200

    Did it!

--- student.py
+++ student.py
@@ -1,16 +1,14 @@
-import time
 class Student:
     def __init__(self,name,hometown,age,height,favoriteIceCream):
-        name = self.name
-        hometown = self.hometown
-        age = self.age
-        height = self.height
-        favoriteIceCream = self.favoriteIceCream
+        self.name = name
+        self.hometown = hometown
+        self.age = age
+        self.height = height
+        self.favoriteIceCream = favoriteIceCream
     def print_summary(self):
-        print("Name: " + self.name + "Age: " + str(self.age) + "Height: " + str(self.height) + "Hometown:" + self.hometown + " Favorite Ice Cream: " + self.favoriteIceCream)
-        
-
+        print("Name: " + self.name + "Age: " + str(self.age) + "Height: "
+              + str(self.height) + "Hometown:" + self.hometown + " Favorite Ice Cream: "
+              + self.favoriteIceCream)
     def get_giraffe_gap(self):
-        return(500 - int(self.height))
-    def get_days_till_birthday(self):
+        return (500 - int(self.height))
         
--- test_student.py
+++ test_student.py
@@ -1,3 +1,4 @@
 from student import Student
-student1 = Student("Ben","Givat Ela" , 15, 173, "Strawberry")
-student1.print_summary(student1)
+student1 = Student("Ben","Givat Ela", 15,173,"Strawberry")
+height = student1.get_giraffe_gap()
+print(height)
